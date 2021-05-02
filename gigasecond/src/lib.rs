2621d5d75f9da4b33c0f88accf28b5fafb9ce550
Nazif Barassounon <nazifbarassounon@gmail.com>
2021-05-02 20:03:56 +0100

    complete "Gigasecond" exercise

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    Utc.timestamp(start.timestamp() + 10i64.pow(9), 0)
+    start + Duration::seconds(1_000_000_000)
 }
