7d82812257b25f0403bec7ce512c4f52ce0cfac1
Nazif Barassounon <nazifbarassounon@gmail.com>
2021-05-09 17:17:49 +0100

    complete "Clock" exercise

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,29 +1,29 @@
-use chrono::{Duration, NaiveTime};
 use std::fmt;
+
+const MINUTES_IN_HOUR: i32 = 60;
+const HOURS_IN_DAY: i32 = 24;
 
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    time: NaiveTime,
+    hours: i32,
+    minutes: i32,
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.time.format("%H:%M"))
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Clock {
-            time: NaiveTime::from_hms(0, 0, 0)
-                + Duration::hours(hours.into())
-                + Duration::minutes(minutes.into()),
+            hours: (hours + minutes.div_euclid(MINUTES_IN_HOUR)).rem_euclid(HOURS_IN_DAY),
+            minutes: minutes.rem_euclid(MINUTES_IN_HOUR),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock {
-            time: self.time + Duration::minutes(minutes.into()),
-        }
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
