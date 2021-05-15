96183b75d211c8e21185a696720aa524c9ee017f
Nazif Barassounon <nazifbarassounon@gmail.com>
2021-05-15 11:12:35 +0100

    complete Anagram exercise (fixes #4)

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,34 +1,22 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut result = HashSet::new();
-    let word_lower = lower_string(word);
-    let word_chars_sorted = sort_chars(&word_lower);
-
-    for anagram in possible_anagrams {
-        let anagram_lower = lower_string(anagram);
-
-        if word_lower == anagram_lower {
-            continue;
-        }
-
-        let anagram_chars_sorted = sort_chars(&anagram_lower);
-
-        if word_chars_sorted == anagram_chars_sorted {
-            result.insert(*anagram);
-        }
-    }
-    result
+    let word_lower = word.to_lowercase();
+    let word_sorted = get_sorted(&word_lower);
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let candidate_lower = candidate.to_lowercase();
+            candidate_lower.len() == word_lower.len()
+                && candidate_lower != word_lower
+                && get_sorted(&candidate_lower) == word_sorted
+        })
+        .copied()
+        .collect()
 }
 
-fn lower_string(word: &str) -> String {
-    word.chars()
-        .map(|c| c.to_lowercase().next().unwrap())
-        .collect::<String>()
-}
-
-fn sort_chars(word: &String) -> Vec<char> {
-    let mut word_chars = word.chars().collect::<Vec<char>>();
-    word_chars.sort_unstable();
-    word_chars
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted: Vec<char> = word.chars().collect();
+    word_sorted.sort_unstable();
+    word_sorted
 }
